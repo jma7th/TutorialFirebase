aea17720eaf5b81d49fc723f9f7560c5b2fa5c59
jma7th <jma7th@gmail.com>
2024-12-09 12:05:19 -0300

    atualização do nested view. não está funcionando.

--- app/src/main/java/com/example/tutorialfirebase/LoginActivity.java
+++ app/src/main/java/com/example/tutorialfirebase/LoginActivity.java
@@ -13,6 +13,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -20,5 +21,7 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        getSupportActionBar().hide();
     }
 }
