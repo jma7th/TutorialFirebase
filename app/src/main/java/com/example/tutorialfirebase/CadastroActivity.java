bc564c92fd4ff8c4b146c7bf39e8e6e7ae84c913
jma7th <jma7th@gmail.com>
2024-12-10 07:40:33 -0300

    criação de mais duas telas. correção do bug.

--- app/src/main/java/com/example/tutorialfirebase/CadastroActivity.java
+++ app/src/main/java/com/example/tutorialfirebase/CadastroActivity.java
@@ -1,9 +1,6 @@
 package com.example.tutorialfirebase;
 
 import android.os.Bundle;
-import android.view.View;
-import android.content.Intent;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Objects;
-
-public class LoginActivity extends AppCompatActivity {
-    private TextView text_tela_cadastro;
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +19,12 @@ public class LoginActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
-        setContentView(R.layout.activity_login);
-        IniciarComponentes();
-
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void IniciarComponentes(){
-        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
--- app/src/main/java/com/example/tutorialfirebase/PerfilActivity.java
+++ app/src/main/java/com/example/tutorialfirebase/PerfilActivity.java
@@ -1,9 +1,6 @@
 package com.example.tutorialfirebase;
 
 import android.os.Bundle;
-import android.view.View;
-import android.content.Intent;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,40 +8,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Objects;
-
-public class LoginActivity extends AppCompatActivity {
-    private TextView text_tela_cadastro;
+public class PerfilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Hide the action bar
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
-
-        setContentView(R.layout.activity_login);
-        IniciarComponentes();
-
         EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_perfil);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void IniciarComponentes(){
-        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
     }
 }
