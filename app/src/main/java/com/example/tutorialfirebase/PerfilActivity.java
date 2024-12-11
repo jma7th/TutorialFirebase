238ef2c14e02355493a90f2296d338a394f6661f
jma7th <jma7th@gmail.com>
2024-12-11 15:52:45 -0300

    configuração na tela de perfil de usuário, que agora atualiza dinâmicamente as informações de nome e endereço de email enquanto o usuário estiver logado.

--- app/src/main/java/com/example/tutorialfirebase/PerfilActivity.java
+++ app/src/main/java/com/example/tutorialfirebase/PerfilActivity.java
@@ -7,13 +7,25 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
 public class PerfilActivity extends AppCompatActivity {
+    private TextView perfil_nome, perfil_email;
     private Button btn_sair;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +48,38 @@ public class PerfilActivity extends AppCompatActivity {
         btn_sair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(PerfilActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null){
+                    perfil_nome.setText(documentSnapshot.getString("nome"));
+                    perfil_email.setText(email);
+                }
+            }
+        });
+    }
+
     private void IniciarComponentes(){
         btn_sair = findViewById(R.id.btn_sair);
+        perfil_nome = findViewById(R.id.perfil_nome);
+        perfil_email = findViewById(R.id.perfil_email);
     }
 }
