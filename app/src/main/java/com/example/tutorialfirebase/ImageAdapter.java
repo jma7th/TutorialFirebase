8a74ac15f963c5a2bda257d4b0a36136aedbd1ed
jma7th <102936397+jma7th@users.noreply.github.com>
2025-01-16 09:25:16 -0300

    adicionado card na tela inicial

---
+++ app/src/main/java/com/example/tutorialfirebase/ImageAdapter.java
@@ -0,0 +1,46 @@
+package com.example.tutorialfirebase;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    private int[] images;
+    private LayoutInflater inflater;
+
+    public ImageAdapter(Context context, int[] images) {
+        this.images = images;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.image_item, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        holder.imageView.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        public ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
--- app/src/main/java/com/example/tutorialfirebase/MainActivity.java
+++ app/src/main/java/com/example/tutorialfirebase/MainActivity.java
@@ -5,14 +5,26 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
 public class MainActivity extends AppCompatActivity {
     private Button btn_perfil;
+    private ViewPager2 viewPager;
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private Runnable runnable;
+    private int[] images = {R.drawable.image1, R.drawable.image2, R.drawable.image3};
+    private int currentPage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +32,21 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         IniciarComponentes();
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageAdapter adapter = new ImageAdapter(this, images);
+        viewPager.setAdapter(adapter);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (currentPage == images.length) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+                handler.postDelayed(this, 5000);
+            }
+        };
+        handler.postDelayed(runnable, 5000);
 
         btn_perfil.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +63,12 @@ public class MainActivity extends AppCompatActivity {
 
     private void IniciarComponentes() {
         btn_perfil = findViewById(R.id.btn_perfil);
+        viewPager = findViewById(R.id.viewPager);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
     }
 }
